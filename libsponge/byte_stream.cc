7783f4e605b2117222a4dde314c4f211803a8e5e
HalcyonAzure <halc-days@outlook.com>
2022-10-29 20:01:24 +0800

    first passed

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -1,6 +1,5 @@
 #include "byte_stream.hh"
 
-#include <iostream>
 // Dummy implementation of a flow-controlled in-memory byte stream.
 
 // For Lab 0, please replace with a real implementation that passes the
@@ -16,9 +15,7 @@ using namespace std;
 ByteStream::ByteStream(const size_t capacity) : buffer_max_size(capacity), buffer(""), input(""), wcnt(0), rcnt(0) {}
 
 size_t ByteStream::write(const string &data) {
-    cout << "Before Write:" << buffer << " with Data:" << data << endl;
     this->input = data;
-    cout << "Input: " << input << endl;
     size_t cnt = 0;
     while (!input_ended() && remaining_capacity() && this->input.length()) {
         this->buffer.push_back(*input.begin());
@@ -27,7 +24,6 @@ size_t ByteStream::write(const string &data) {
     }
     // 统计写入数据
     this->wcnt += cnt;
-    cout << "After Write:" << buffer << endl;
     return cnt;
     // DUMMY_CODE(data);
     // return {};
@@ -47,9 +43,7 @@ string ByteStream::peek_output(const size_t len) const {
 
 //! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    cout << "Before Pop:" << buffer << " Pop len:" << len << endl;
     buffer.erase(0, len);
-    cout << "After Pop:" << buffer << endl;
     this->rcnt += len;
 }
 
