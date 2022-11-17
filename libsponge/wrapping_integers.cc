e5c2e9679490977bc54d3e7a5129d699c356dd3e
HalcyonAzure <halc-days@outlook.com>
2022-11-17 11:22:18 +0800

    ctest pass wrap

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t result = (n + isn.raw_value()) % (static_cast<uint64_t>(UINT32_MAX) + 1);
+    return WrappingInt32(static_cast<uint32_t>(result));
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +29,15 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    const uint64_t L = (1ul << 32);
+    const int64_t a = (checkpoint / L) * L + (n.raw_value() - isn.raw_value()) % L;
+    if (checkpoint > a + L / 2) {
+        return a + L;
+    } else {
+        if (checkpoint < L) {
+            return n.raw_value() < isn.raw_value() ? a + L : a;
+        } else {
+            return checkpoint < a - L / 2 ? a - L : a;
+        }
+    }
 }
